1bd665fdb24ff2c65105d9865def35fb8c24339a
henmerlin <henmerlin>
2016-11-21 09:23:54 -0200

    Commit Model

---
+++ src/br/net/gvt/portalefika/SubAbaPortal.java
@@ -0,0 +1,44 @@
+package src.br.net.gvt.portalefika;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "PE_SUBABA")
+public class SubAbaPortal extends ComponentePortal {
+    
+    @OneToMany(fetch = FetchType.EAGER, mappedBy="subAba", cascade=CascadeType.REFRESH)
+    private List<Conteudo> conteudos;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @NotNull
+    private AbaPortal abaPortal;
+
+    public SubAbaPortal() {
+    }
+
+    public void setConteudos(List<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    public List<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setAbaPortal(AbaPortal abaPortal) {
+        this.abaPortal = abaPortal;
+    }
+
+    public AbaPortal getAbaPortal() {
+        return abaPortal;
+    }
+}
