8e38c8e25f3891a9fd05bdc4c80721d4f31cb6e0
G0042204 <G0042204@NOTCTARO8-5078.gvt.net.br>
2016-12-19 15:33:58 -0200

    Fix #1 Collections.sort

--- Anjo/JavaSource/controllers/sistema/DuvidaBean.java
+++ Anjo/JavaSource/controllers/sistema/DuvidaBean.java
@@ -7,86 +7,89 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import entidades.sistema.Duvida;
+import java.util.Arrays;
+import java.util.Collections;
 import models.sistema.DuvidaServico;
 import util.JSFUtil;
 
 @ManagedBean
 @ViewScoped
 public class DuvidaBean {
-	
-	private Duvida duvida;
-	
-	private Duvida duvidaModifica;
-	
-	@EJB
-	private DuvidaServico duvidaServico;
 
-	public DuvidaBean() {
-		
-		this.duvida = new Duvida();
-		
-		this.duvidaModifica = new Duvida();
-		
-	}
-	
-	public void cadastrarDuvida() {
-		
-		try {
-			
-			this.duvidaServico.cadastrarDuvida(this.duvida);
-			JSFUtil.addInfoMessage("Duvida cadastrada com sucesso.");
-			this.duvida = new Duvida();
-			
-		} catch (Exception e) {
+    private Duvida duvida;
 
-			JSFUtil.addErrorMessage(e.getMessage());
-			
-		}
-		
-	}
-	
-	public void modificaDuvida() {
-		
-		try {
-			
-			this.duvidaServico.modificarDuvida(this.duvidaModifica);
-			JSFUtil.addInfoMessage("Duvida modificada com sucesso.");
-			this.duvidaModifica = new Duvida();
-			
-		} catch (Exception e) {
+    private Duvida duvidaModifica;
 
-			JSFUtil.addErrorMessage(e.getMessage());
-			
-		}
-		
-	}
-	
-	public List<Duvida> listaTodasDuvidas() {
-		
-		return this.duvidaServico.listaTodasDuvidas();
-		
-	}
-	
-	public List<Duvida> listaDuvidasLista(Boolean lista, Boolean ativo) {
-		
-		return this.duvidaServico.listaDuvidasLista(lista, ativo);
-		
-	}
+    @EJB
+    private DuvidaServico duvidaServico;
 
-	public Duvida getDuvida() {
-		return duvida;
-	}
+    public DuvidaBean() {
 
-	public void setDuvida(Duvida duvida) {
-		this.duvida = duvida;
-	}
+        this.duvida = new Duvida();
 
-	public Duvida getDuvidaModifica() {
-		return duvidaModifica;
-	}
+        this.duvidaModifica = new Duvida();
 
-	public void setDuvidaModifica(Duvida duvidaModifica) {
-		this.duvidaModifica = duvidaModifica;
-	}	
-	
+    }
+
+    public void cadastrarDuvida() {
+
+        try {
+
+            this.duvidaServico.cadastrarDuvida(this.duvida);
+            JSFUtil.addInfoMessage("Duvida cadastrada com sucesso.");
+            this.duvida = new Duvida();
+
+        } catch (Exception e) {
+
+            JSFUtil.addErrorMessage(e.getMessage());
+
+        }
+
+    }
+
+    public void modificaDuvida() {
+
+        try {
+
+            this.duvidaServico.modificarDuvida(this.duvidaModifica);
+            JSFUtil.addInfoMessage("Duvida modificada com sucesso.");
+            this.duvidaModifica = new Duvida();
+
+        } catch (Exception e) {
+
+            JSFUtil.addErrorMessage(e.getMessage());
+
+        }
+
+    }
+
+    public List<Duvida> listaTodasDuvidas() {
+
+        return this.duvidaServico.listaTodasDuvidas();
+
+    }
+
+    public List<Duvida> listaDuvidasLista(Boolean lista, Boolean ativo) {
+        List<Duvida> ds = this.duvidaServico.listaDuvidasLista(lista, ativo);
+        Collections.sort(ds);
+        return ds;
+
+    }
+
+    public Duvida getDuvida() {
+        return duvida;
+    }
+
+    public void setDuvida(Duvida duvida) {
+        this.duvida = duvida;
+    }
+
+    public Duvida getDuvidaModifica() {
+        return duvidaModifica;
+    }
+
+    public void setDuvidaModifica(Duvida duvidaModifica) {
+        this.duvidaModifica = duvidaModifica;
+    }
+
 }
--- Anjo/JavaSource/entidades/sistema/Duvida.java
+++ Anjo/JavaSource/entidades/sistema/Duvida.java
@@ -6,79 +6,90 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="anjo_Duvida")
-public class Duvida {
+@Table(name = "anjo_Duvida")
+public class Duvida implements Comparable<Duvida> {
 
-	@Id
-	@GeneratedValue
-	private Integer id;
-	
-	private String nome;
-	
-	private Boolean lista;
-	
-	private Boolean ativo = false;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
+    private String nome;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    private Boolean lista;
 
-	public String getNome() {
-		return nome;
-	}
+    private Boolean ativo = false;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public Boolean getLista() {
-		return lista;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setLista(Boolean lista) {
-		this.lista = lista;
-	}	
+    public String getNome() {
+        return nome;
+    }
 
-	public Boolean getAtivo() {
-		return ativo;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
-	}
+    public Boolean getLista() {
+        return lista;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public void setLista(Boolean lista) {
+        this.lista = lista;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Duvida other = (Duvida) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    public Boolean getAtivo() {
+        return ativo;
+    }
 
-	@Override
-	public String toString() {
-		return "Duvida [id=" + id + "]";
-	}	
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Duvida other = (Duvida) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Duvida [id=" + id + "]";
+    }
+
+    @Override
+    public int compareTo(Duvida d) {
+        //ascending order
+        return this.nome.toUpperCase().compareTo(d.getNome().toUpperCase());
+    }
 
 }
