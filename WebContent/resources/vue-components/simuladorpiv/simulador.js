0e0c7693d0a7732c514436782a30c6470fe970c7
henmerlin <henmerlin@gmail.com>
2017-01-02 10:02:34 -0200

    Simulador

--- WebContent/resources/vue-components/simuladorpiv/simulador.js
+++ WebContent/resources/vue-components/simuladorpiv/simulador.js
@@ -58,7 +58,7 @@ var Form = {
                     <div class=\"form-group\">\n\
                         <label for=\"fcr\">FCR<\/label>\n\
                         <div class=\"input-group\">\n\
-                            <input id=\"fcr\" type=\"number\" v-model=\"vm.fcr\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"FCR\" aria-describedby=\"fcr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"fcr-addon1\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"adr\">Ader\u00EAncia<\/label>\r\n            <div class=\"input-group\">\r\n                <input id=\"adr\" v-model=\"vm.adr\" type=\"number\"  min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"Ader\u00EAncia\" aria-describedby=\"adr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"adr-addon1\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"monitoria\">Monitoria<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.monitoria\"  id=\"monitoria\" type=\"number\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"Monitoria\" aria-describedby=\"fcr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"monitoria-addon1\"><span class=\"glyphicon glyphicon-headphones\" aria-hidden=\"true\"><\/span><\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"tma\">TMA<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.tma\" id=\"tma\" type=\"time\" class=\"form-control\" placeholder=\"TMA\" aria-describedby=\"tma-addon1\">\r\n                <span class=\"input-group-addon\" id=\"basic-tma\"><span class=\"glyphicon glyphicon-dashboard\" aria-hidden=\"true\"><\/span><\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"tma\">GPS<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.gps\" id=\"gps\" type=\"number\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"GPS\" aria-describedby=\"tma-addon1\">\r\n                <span class=\"input-group-addon\" id=\"basic-tma\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-6\">\r\n        <div class=\"panel panel-default\">\r\n            <div class=\"panel-heading\">\r\n                <span>Target<\/span>\r\n            <\/div>\r\n            <div class=\"panel-body\">\r\n                <h2 class=\"text-center\"><span v-text=\"vm.piv.target\"><\/span>%<\/h2>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <button type=\"button\" class=\"btn btn-warning\">Limpar<\/button>\r\n    <\/div>\r\n\r\n<\/div>'
+                            <input id=\"fcr\" type=\"number\" v-model=\"vm.fcr\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"FCR\" aria-describedby=\"fcr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"fcr-addon1\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"adr\">Ader\u00EAncia<\/label>\r\n            <div class=\"input-group\">\r\n                <input id=\"adr\" v-model=\"vm.adr\" type=\"number\"  min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"Ader\u00EAncia\" aria-describedby=\"adr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"adr-addon1\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"monitoria\">Monitoria<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.monitoria\"  id=\"monitoria\" type=\"number\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"Monitoria\" aria-describedby=\"fcr-addon1\">\r\n                <span class=\"input-group-addon\" id=\"monitoria-addon1\"><span class=\"glyphicon glyphicon-headphones\" aria-hidden=\"true\"><\/span><\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"tma\">TMA<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.tma\" id=\"tma\" type=\"time\" class=\"form-control\" placeholder=\"TMA\" aria-describedby=\"tma-addon1\">\r\n                <span class=\"input-group-addon\" id=\"basic-tma\"><span class=\"glyphicon glyphicon-dashboard\" aria-hidden=\"true\"><\/span><\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <div class=\"form-group\">\r\n            <label for=\"tma\">GPS<\/label>\r\n            <div class=\"input-group\">\r\n                <input v-model=\"vm.gps\" id=\"gps\" type=\"number\" min=\"0\" max=\"100\" class=\"form-control\" placeholder=\"GPS\" aria-describedby=\"tma-addon1\">\r\n                <span class=\"input-group-addon\" id=\"basic-tma\">%<\/span>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-6\">\r\n        <div class=\"panel panel-default\">\r\n            <div class=\"panel-heading\">\r\n                <span>Target<\/span>\r\n            <\/div>\r\n            <div class=\"panel-body\">\r\n                <h2 class=\"text-center\"><span v-text=\"vm.piv.target * 100\"><\/span>%<\/h2>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"col-xs-3\">\r\n        <button type=\"button\" class=\"btn btn-warning\">Limpar<\/button>\r\n    <\/div>\r\n\r\n<\/div>'
     , data: function() {
         return data
     }
@@ -66,7 +66,9 @@ var Form = {
 
 var DadosUsuario = {
     template:
-            '<table class=\"table small table-bordered table-condensed\">\r\n    <tbody>\r\n        <tr>\r\n            <th><label for=\"nome\">Nome:<\/label><\/th>\r\n            <td><span id=\"nome\" v-text=\"vm.piv.op.nome\"><\/span><\/td>\r\n        <\/tr>\r\n        <tr>\r\n            <th><label for=\"super\">Supervis\u00E3o:<\/label><\/th>\r\n            <td><span id=\"super\" v-text=\"vm.piv.op.nomeSupervisor\"><\/span><\/td>\r\n        <\/tr>\r\n\r\n        <tr>\r\n            <th><label for=\"equipe\">Equipe:<\/label><\/th>\r\n            <td><span id=\"equipe\" v-text=\"vm.piv.op.equipe\"><\/span><\/td>\r\n        <\/tr>\r\n    <\/tbody>\r\n<\/table>',
+            '<table class=\"table small table-bordered table-condensed\">\r\n    <tbody>\r\n        <tr>\r\n            <th class=\"row\"><label for=\"nome\">Nome:<\/label><\/th>\r\n            <td><span id=\"nome\" v-text=\"vm.piv.op.nome\"><\/span><\/td>\r\n        <\/tr>\r\n        <tr>\r\n            <th class=\"row\"><label for=\"super\">Supervis\u00E3o:<\/label><\/th>\r\n            <td><span id=\"super\" v-text=\"vm.piv.op.nomeSupervisor\"><\/span><\/td>\r\n        <\/tr>\r\n\r\n        <tr>\r\n            <th class=\"row\"><label for=\"equipe\">Equipe:<\/label><\/th>\r\n            <td><span id=\"equipe\" v-text=\"vm.piv.op.equipe\"><\/span><\/td>\r\n        <\/tr>\r\n    <\/tbody>\r\n<\/table>'
+    ,
+
     data: function() {
         return data
     }
@@ -111,14 +113,23 @@ new Vue({
             $.get(pivURL + self.usuario.login, function(data) {
                 self.usuario.piv = data.calculoPivFacade;
                 self.vm.piv = data.calculoPivFacade;
+
+                // FCR
                 var _fcr = self.getIndicadorPorNome("FCR").realizado * 100;
                 if (_fcr) {
                     self.vm.fcr = _fcr;
                 }
 
+                // TMA
                 var _tma = moment("1900-01-01 00:00:00").add(self.getIndicadorPorNome("TMA").realizado, 'seconds').format("HH:mm:ss");
                 if (_tma) {
                     self.vm.tma = _tma;
+                }
+
+                // ADERENCIA
+                var _adr = self.getIndicadorPorNome("ADERENCIA").realizado * 100;
+                if (_adr) {
+                    self.vm.adr = _adr;
                 }
 
             });
