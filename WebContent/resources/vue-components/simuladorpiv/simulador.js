5a27b69b3f6fcf8f3e0e9253bec62093bc552dac
henmerlin <henmerlin@gmail.com>
2017-01-02 18:35:10 -0200

    Simulador Refatorado

--- WebContent/resources/vue-components/simuladorpiv/simulador.js
+++ WebContent/resources/vue-components/simuladorpiv/simulador.js
@@ -4,6 +4,11 @@ var pivURL = "http://localhost:8080/pivAPI/operador/simulador/";
 var sessionURL = "/session/";
 var data =
         {
+            "fcr": {"nome": "FCR"},
+            "adr": {"nome": "ADERENCIA"},
+            "monitoria": {"nome": "MONITORIA"},
+            "tma": {"nome": "TMA"},
+            "gps": {"nome": "GPS"},
             show: false,
             usuario: {
                 piv:
@@ -104,12 +109,39 @@ new Vue({
                 }
             });
         },
+        getTarget: function() {
+            var self = this;
+
+
+            var simulator =
+                    {"simuladorAtendimento":
+                                {op: self.vm.piv.op},
+                        "fcr": {"realizado": (self.vm.fcr / 100)},
+                        "adr": {"realizado": (self.vm.adr / 100)},
+                        "tma": {"realizado": (self.vm.tma / 100)},
+                        "monitoria": {"realizado": (self.vm.fcr / 100)},
+                        "gps": {"realizado": (self.vm.fcr / 100)},
+                    };
+
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                data: JSON.stringify(simulator),
+                url: pivURL + "/change/",
+                success: function(data) {
+                    console.log(data)
+                },
+                complete: function(jqXHR, textStatus) {
+                    console.log(jqXHR)
+                }
+            });
+        },
         loadIndicadores: function() {
             var self = this;
             $.get(pivURL + self.usuario.login, function(data) {
                 self.usuario.piv = data.calculoPivFacade;
                 self.vm.piv = data.calculoPivFacade;
-
                 // FCR
                 var _fcr = self.getIndicadorPorNome("FCR").realizado * 100;
                 if (_fcr) {
